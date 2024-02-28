4c24faad78d70c8aaeae577634457342eb744f8c
SeongEunGyu <116744770+Dyedds@users.noreply.github.com>
2024-02-28 22:53:46 +0900

    Add files via upload

---
+++ 5주차/성은규/섹션5/일기 리스트 저장.js
@@ -0,0 +1,64 @@
+import React, {useRef, useState} from "react";
+import './App.css';
+import DiaryEditor from './DiaryEditor';
+import DiaryList from './DiaryList';
+
+// const dummyList = [
+//   {
+//     id:1,
+//     author:"성은규",
+//     content:"하이 1",
+//     emotion:2,
+//     created_date: new Date().getTime(), //Date객체(시간)를 밀리세컨드로 전환
+//   },
+//   {
+//     id:2,
+//     author:"허성지",
+//     content:"하이 2",
+//     emotion:3,
+//     created_date: new Date().getTime(), //Date객체(시간)를 밀리세컨드로 전환
+//   },
+//   {
+//     id:3,
+//     author:"전현민",
+//     content:"하이 3",
+//     emotion:4,
+//     created_date: new Date().getTime(), //Date객체(시간)를 밀리세컨드로 전환
+//   },
+//   {
+//     id:4,
+//     author:"황동현",
+//     content:"하이 4",
+//     emotion:1,
+//     created_date: new Date().getTime(), //Date객체(시간)를 밀리세컨드로 전환
+//   }
+// ]
+
+const App = () => {
+  
+  const [data,setData] = useState([]);
+
+  const dataId = useRef(0)
+
+  const onCreate = (author, content, emotion) =>{
+    const created_date = new Date().getTime();
+    const newItem = {
+      author,
+      content,
+      emotion,
+      created_date,
+      id : dataId.current
+    }
+    dataId.current += 1;
+    setData([newItem, ...data]);
+  }
+
+  return (
+    <div className="App">
+      <DiaryEditor onCreate={onCreate}/>
+      <DiaryList diaryList={data}/>
+    </div>
+  );
+};
+
+export default App;
